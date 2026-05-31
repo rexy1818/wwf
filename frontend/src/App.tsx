8c85c81d9edd698f5f27d4f1cb721ed58f7aa0ce
rexy1818 <ramirojordanjurado@gmail.com>
2026-05-30 22:06:21 -0400

    Implementacion de barra lateral

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,16 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/home";
 import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import Videos from "./pages/videos";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/videos" element={<Videos />} />
     </Routes>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "./styles/toast.css";
+import "react-toastify/dist/ReactToastify.css";
+
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+
+      <ToastContainer
+        position="top-right"
+        autoClose={2500}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="light"
+      />
     </BrowserRouter>
   </React.StrictMode>
 );
